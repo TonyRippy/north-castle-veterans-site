dd9c002e5018c84b9fc75764b5c561a435dae5ea
Tony Rippy <tonyrippy@gmail.com>
2017-09-01 15:14:36 -0400

    Updated CemeteriesServlet to use new Cemetery class.

--- src/main/java/myapp/CemeteriesServlet.java
+++ src/main/java/myapp/CemeteriesServlet.java
@@ -1,45 +1,26 @@
 package myapp;
 
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.Query;
-import com.google.cloud.datastore.QueryResults;
-import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.List;
+
 public class CemeteriesServlet extends HttpServlet {
+  private static class Output {
+    List<Cemetery> cemeteries;
+  }
+  
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
-    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-
-    StringBuilder json = new StringBuilder("{\"cemeteries\":[");
-    Query<Entity> query = Query.newEntityQueryBuilder()
-        .setKind("Cemetery")
-        .build();
-    QueryResults<Entity> people = datastore.run(query);
-    boolean first = true;
-    while (people.hasNext()) {
-        Entity person = people.next();
-        if (first) {
-            first = false;
-        } else {
-            json.append(',');
-        }
-        json.append("{\"id\": \"")
-            .append(person.getKey().getName())
-            .append("\", \"name\": \"")
-            .append(person.getString("cemeteryName"))
-            .append("\"}");
-    }
-    json.append("]}");
+    Output out = new Output();
+    out.cemeteries = Cemetery.listAll();
     resp.setContentType("application/json");
     resp.setCharacterEncoding("UTF-8");
-    resp.getWriter().print(json);
+    new Gson().toJson(out, resp.getWriter());
   }
 }
