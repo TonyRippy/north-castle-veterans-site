da8c8c868a78d1d5fc7948ded40397abeab3cd51
Tony Rippy <tonyrippy@gmail.com>
2017-09-01 16:38:52 -0400

    Added some protection against running the debug handler in prod.

--- src/main/java/myapp/AddCemeteryServlet.java
+++ src/main/java/myapp/AddCemeteryServlet.java
@@ -1,17 +1,22 @@
 package myapp;
 
+import static myapp.Config.isTest;
+
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.List;
-
 public class AddCemeteryServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    if (!isTest()) {
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
     Cemetery c = new Cemetery("Test");
     c.name = "St. Test";
     c.writeToDatastore();
--- src/main/java/myapp/Config.java
+++ src/main/java/myapp/Config.java
@@ -6,22 +6,30 @@ import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.http.HttpTransportOptions;
 
-class Config {
+final class Config {
+  private Config() {}
+ 
   private static DatastoreOptions datastoreOptions = null;
-  
+
+  public static boolean isTest() {
+    return SystemProperty.environment.value() != SystemProperty.Environment.Value.Production;
+  }
+
   public static synchronized DatastoreOptions getDatastoreOptions() {
     if (datastoreOptions == null) {
-      if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
-        // Production
-        datastoreOptions = DatastoreOptions.getDefaultInstance();
-      } else {
-        // Local development server
+      if (isTest()) {
+        // Use local development server.
+        // You can start one by using the following command:
+        // gcloud beta emulators datastore start --project=north-castle-veterans-site --host-port=localhost:9999
         datastoreOptions = DatastoreOptions.newBuilder()
           .setHost("localhost:9999")
           .setProjectId("north-castle-veterans-site")
           .setTransportOptions(HttpTransportOptions.newBuilder().build())
           .setCredentials(NoCredentials.getInstance())
           .build();
+      } else {
+        // Production
+        datastoreOptions = DatastoreOptions.getDefaultInstance();
       }
     }
     return datastoreOptions;
