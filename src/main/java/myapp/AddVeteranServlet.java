ebf3f5d75f65a7db6d74e19794fb4a19ec94b8ec
Tony Rippy <tonyrippy@gmail.com>
2017-09-01 23:06:30 -0400

    Added ability to update veterans.

---
+++ src/main/java/myapp/AddVeteranServlet.java
@@ -0,0 +1,28 @@
+package myapp;
+
+import static myapp.Config.isTest;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddVeteranServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    if (!isTest()) {
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
+    
+    Veteran v = new Veteran("Johnny_Appleseed", "Test");
+    v.givenName = "Johnny Appleseed";
+    v.writeToDatastore();
+    
+    resp.setContentType("text/plain");
+    resp.getWriter().print("OK");
+  }
+}
--- src/main/java/myapp/Veteran.java
+++ src/main/java/myapp/Veteran.java
@@ -3,6 +3,7 @@ package myapp;
 import static myapp.Config.getDatastore;
 
 import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreException;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
@@ -39,6 +40,13 @@ public class Veteran {
     return new Veteran(veteranId, cemeteryId);
   }
 
+  private Key buildKey(KeyFactory keyFactory) {
+    return keyFactory
+      .addAncestor(PathElement.of("Cemetery", cemeteryId))
+      .setKind("Veteran")
+      .newKey(id);
+  }
+
   boolean readSummary(Entity e) {
     givenName = e.getString("givenName");
     return true;
@@ -49,12 +57,16 @@ public class Veteran {
     return true;
   }
   
+  private Entity toEntity(KeyFactory keyFactory) {
+    Key key = buildKey(keyFactory);
+    return Entity.newBuilder(key)
+      .set("givenName", givenName)
+      .build();
+  }
+
   public boolean readFromDatastore() {
     Datastore datastore = getDatastore();
-    Key key = datastore.newKeyFactory()
-      .addAncestor(PathElement.of("Cemetery", cemeteryId))
-      .setKind("Veteran")
-      .newKey(id);
+    Key key = buildKey(datastore.newKeyFactory());
     Query<Entity> query = Query.newEntityQueryBuilder()
       .setKind("Veteran")
       .setFilter(PropertyFilter.eq("__key__", key))
@@ -63,6 +75,18 @@ public class Veteran {
     if (results.hasNext()) {
       return readFull(results.next());
     } else {
+      return false;
+    }
+  }
+
+  public boolean writeToDatastore() {
+    Datastore datastore = getDatastore();
+    Entity entity = toEntity(datastore.newKeyFactory());
+    try {
+      datastore.put(entity);
+      return true;
+    } catch (DatastoreException ex) {
+      // TODO: Log the error?
       return false;
     }
   }
